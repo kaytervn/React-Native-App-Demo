cc8d9d5cb2dbc5d57a0708110935e2f30726412a
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2024-10-03 00:32:42 +0700

    update color text Chat

--- src/app/views/chat/ChatList.tsx
+++ src/app/views/chat/ChatList.tsx
@@ -48,14 +48,14 @@ const ChatList = ({ navigation }: any) => {
   return (
     <SafeAreaView className="flex-1 bg-white">
       {/* Header */}
-      <View className="p-4 border-b border-gray-300">
-        <View className="flex-row items-center">
+      <View className="p-4 border-b border-gray-300 bg-sky-500">
+        <View className="flex-row items-center ">
           <TextInput
-            className="flex-1 bg-gray-200 rounded-full px-4 py-2"
+            className="flex-1 bg-white rounded-full px-4 py-2"
             placeholder="Tìm kiếm tin nhắn"
           />
           <TouchableOpacity className="ml-2">
-            <SettingsIcon size={24} />
+            <SettingsIcon size={24} className="text-white" />
           </TouchableOpacity>
         </View>
 
